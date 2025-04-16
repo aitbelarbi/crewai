86ae4105838b48e60a55c1a1b2211dec798e3f52
aitbelarbi <ait.belarbi@gmail.com>
2025-04-16 17:53:55 +0200

    feat: new agents

--- src/copyright/crew.py
+++ src/copyright/crew.py
@@ -18,22 +18,6 @@ class Copyright():
     agents_config = 'config/agents.yaml'
     tasks_config = 'config/tasks.yaml'
 
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
- #   @agent
- #   def researcher(self) -> Agent:
- #       return Agent(
- #           config=self.agents_config['researcher'],
- #           verbose=True
- #       )
-
- #   @agent
- #   def reporting_analyst(self) -> Agent:
- #       return Agent(
- #           config=self.agents_config['reporting_analyst'],
- #           verbose=True
- #       )
-    
     @agent
     def recipe_researcher(self) -> Agent:
         return Agent(
@@ -49,25 +33,9 @@ class Copyright():
             tools=[web_rag_tool],
             verbose=True
         )
-
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
- #   @task
- #   def research_task(self) -> Task:
- #       return Task(
- #           config=self.tasks_config['research_task'],
- #       )
-
- #   @task
- #   def reporting_task(self) -> Task:
- #       return Task(
- #           config=self.tasks_config['reporting_task'],
- #           output_file='report.md'
- #       )
-
+    
     @task
-    def task_research_recipe_here(self) -> Task:
+    def task_research_recipe(self) -> Task:
         return Task(
             config=self.tasks_config['task_research_recipe'],
             output_file='report.md'
@@ -94,3 +62,36 @@ class Copyright():
             verbose=True,
             # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
+
+    # If you would like to add tools to your agents, you can learn more about it here:
+    # https://docs.crewai.com/concepts/agents#agent-tools
+ #   @agent
+ #   def researcher(self) -> Agent:
+ #       return Agent(
+ #           config=self.agents_config['researcher'],
+ #           verbose=True
+ #       )
+
+ #   @agent
+ #   def reporting_analyst(self) -> Agent:
+ #       return Agent(
+ #           config=self.agents_config['reporting_analyst'],
+ #           verbose=True
+ #       )
+    
+
+    # To learn more about structured task outputs,
+    # task dependencies, and task callbacks, check out the documentation:
+    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
+ #   @task
+ #   def research_task(self) -> Task:
+ #       return Task(
+ #           config=self.tasks_config['research_task'],
+ #       )
+
+ #   @task
+ #   def reporting_task(self) -> Task:
+ #       return Task(
+ #           config=self.tasks_config['reporting_task'],
+ #           output_file='report.md'
+ #       )
