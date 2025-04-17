cfe5f761d512c618a250dc70393e75579baf998b
aitbelarbi <ait.belarbi@gmail.com>
2025-04-17 14:49:24 +0200

    feat: new agents

--- src/copyright/crew.py
+++ src/copyright/crew.py
@@ -34,6 +34,13 @@ class Copyright():
         )
     
     @agent
+    def recipe_writer(self) -> Agent:
+        return Agent(
+            config=self.agents_config['recipe_writer'],
+            verbose=True
+        )
+
+    @agent
     def seo_specialist(self) -> Agent:
         return Agent(
             config=self.agents_config['seo_specialist'],
@@ -46,6 +53,13 @@ class Copyright():
         return Task(
             config=self.tasks_config['task_research_recipe'],
             output_file='report.md'
+        )
+
+    @task
+    def task_write_recipe(self) -> Task:
+        return Task(
+            config=self.tasks_config['task_write_recipe'],
+            output_file='recipe_article.md'
         )
 
     @task
