7f2d29ce1b6a76a47567fceeb5413aa865408d0b
aitbelarbi <ait.belarbi@gmail.com>
2025-04-17 17:26:56 +0200

    feat: new agents

--- src/copyright/main.py
+++ src/copyright/main.py
@@ -17,13 +17,8 @@ def run():
     """
     Run the crew.
     """
-    inputs = {
-        'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
-    }
-    
     try:
-        Copyright().crew().kickoff(inputs=inputs)
+        Copyright().crew().kickoff()
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
 
